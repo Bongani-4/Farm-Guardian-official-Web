4dc2e10958aeeffcb4ac144e0817fb8cda7c4c3f
Bongani-4 <njathubongani@gmail.com>
2024-04-16 17:40:39 +0200

    Router set up

---
+++ FG/src/Pages/AnimalCaretaker.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AnimalCaretaker = () => {
+  return (
+    <div className="text-Green"> HOme page</div>
+  )
+}
+
+export default AnimalCaretaker
--- FG/tailwind.config.js
+++ FG/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#054a15",
+        "Green": "#0aa32d"
+      }
+    },
   },
   plugins: [],
 }
