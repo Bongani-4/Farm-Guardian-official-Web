099baf3c4f9dd1262ed9ffabd7c5a993c12609fa
Bongani-4 <njathubongani@gmail.com>
2024-04-16 17:14:39 +0200

    Router set up

--- FG/src/Router/Router.jsx
+++ FG/src/Router/Router.jsx
@@ -2,15 +2,16 @@
 import {createBrowserRouter} from "react-router-dom";
 import Home from "../Pages/Home";
 import App from "../App";
+import About from "../Pages/About";
 const router = createBrowserRouter([
     {
       path: "/",
       element: <App/>,
       children: [
-        {path: "/",
-        element: <Home/>
+        {path: "/", element: <Home/>},
+        
 
-      }
+      
       ]
     },
   ]);
