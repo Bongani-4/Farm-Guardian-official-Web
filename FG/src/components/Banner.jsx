8f36b71a5ac3a14c760437b85c904e7524fabb3e
Bongani-4 <njathubongani@gmail.com>
2024-04-17 19:33:32 +0200

    animal cartaker set up input

--- FG/src/components/Banner.jsx
+++ FG/src/components/Banner.jsx
@@ -4,8 +4,18 @@ const Banner = () => {
   return (
   <div className='max-w-screen-2xl container mx-auto xl:px-0 px-4 md:py-10 py-14'> 
     <h1 className='text-5xl font-bold text-black mb-3 text-left'>Find your<span className='text-green'> job here</span></h1>
-     <p className='text-lg'> Find thousands of jobs in agriculture ,tech, business and many more sectors. </p>
-  </div>
+     <p className='text-lg text-black mb-8' > Find thousands of jobs in agriculture, tech, business and many more sectors waiting for you. </p>
+
+     <form>
+             <div>
+              <div>
+                 <input type="text" name="title" id="title" className='block flex-1 border-0 bg-transparent py-1.5 pl-8 text=gray-900  placeholder:text=gray-400 
+                  focus-right-0 sm:text-sm sm:leading-6' />
+              </div>
+             </div>
+     </form>
+  </div> 
+   
     
 );
 
