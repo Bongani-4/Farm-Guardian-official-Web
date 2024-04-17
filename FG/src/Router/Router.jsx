889fa8a914880b63d663a6aa3009f5d5391481e3
Bongani-4 <njathubongani@gmail.com>
2024-04-17 10:47:05 +0200

    Navbar set up

--- FG/src/Router/Router.jsx
+++ FG/src/Router/Router.jsx
@@ -3,12 +3,14 @@ import {createBrowserRouter} from "react-router-dom";
 import Home from "../Pages/Home";
 import App from "../App";
 import About from "../Pages/About";
+import AnimalCaretaker from "../Pages/AnimalCaretaker";
 const router = createBrowserRouter([
     {
       path: "/",
       element: <App/>,
       children: [
         {path: "/", element: <Home/>},
+        {path: "/AnimalCaretaker", element: <AnimalCaretaker/> },
         
 
       
--- FG/src/components/Navbar.jsx
+++ FG/src/components/Navbar.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom';
+
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMenuToggler = () => {
+
+    setIsMenuOpen(!isMenuOpen) 
+  }
+  const navItems =[
+
+    {path: "/", title: "Search here"},
+    {path: "/my-job", title: "My Jobs"},
+    {path: "/salary", title: "salary Estimated"},
+    {path: "/post-job", title: "Post a job"},
+  ]
   return (
-    <div>Navbar</div>
+    <header>                          
+      <nav>
+          <a href="/" className='flex items-center gap-2 text-2xl' > Job opportunities</a>
+            {/*for larger devces */}
+             <ul className='hidden md:flex gap-12'>
+                  {
+                    navItems.map(({path, title}) => (
+                        <li key={path}  className='text-base text-primary'>
+                             <NavLink
+                               to={path}
+                                className={({ isActive }) =>  isActive ? "active" : "" }  
+                              >
+                             {title}
+                              </NavLink> 
+
+                        </li>
+                    )  )
+                  }
+             </ul>
+             {/*register and login  */}
+             <div className='text-base text-primary font-medium space-x-5 hidden lg:block'>
+              
+              
+             </div>
+      </nav>
+    </header>
   )
 }
 
