426c1cb81323fbde9ef45d3c333306c29ff4fc90
Bongani-4 <njathubongani@gmail.com>
2024-04-17 19:09:06 +0200

    animal cartaker set up

--- FG/src/components/Banner.jsx
+++ FG/src/components/Banner.jsx
@@ -4,7 +4,9 @@ const Banner = () => {
   return (
   <div className='max-w-screen-2xl container mx-auto xl:px-0 px-4 md:py-10 py-14'> 
     <h1 className='text-5xl font-bold text-black mb-3 text-left'>Find your<span className='text-green'> job here</span></h1>
+     <p className='text-lg'> Find thousands of jobs in agriculture ,tech, business and many more sectors. </p>
   </div>
+    
 );
 
 }
