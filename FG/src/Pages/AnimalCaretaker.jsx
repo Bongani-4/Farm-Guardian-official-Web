bf0bc987fe5a1ecc90307bcb2fb727d24bc5ac84
Bongani-4 <njathubongani@gmail.com>
2024-04-18 15:09:53 +0200

    animal cartaker set up input

--- FG/src/Pages/AnimalCaretaker.jsx
+++ FG/src/Pages/AnimalCaretaker.jsx
@@ -1,13 +1,35 @@
-import  { useState } from "react";
+import  { useEffect, useState } from "react";
 import Navbar from '../components/Navbar'
 import Banner from '../components/Banner';
 
 const AnimalCaretaker = () => {
+
+  const [selectedCategory, setSelectedCategoryy]= useState(null);
+
+  const [jobs,setJobs] = useState([]);
+
+
+  useEffect(() => {
+
+    fetch("jobs.json").then( res => res.json()).then(data => {
+    
+      setJobs(data)
+    })
+  },[])
+
+  //  handling input data
   const [query, setQuery] = useState("");
   const handleInputChange = (event) =>
    { setQuery(event.target.value)
     
   }
+
+  //  filtring jobs based on title
+
+  const filteredItems = jobs.filter((job) => job.jobTitle.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+  console.log(filteredItems);
+
+
   return (
     <div className="text-Green"> 
     <Navbar/>
