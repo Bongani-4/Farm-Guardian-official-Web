46ac028eb9733aa05c8a68008ab5034df0d16ad3
Bongani-4 <njathubongani@gmail.com>
2024-04-18 17:54:37 +0200

    UI set up jobs data filtered

--- FG/src/components/Card.jsx
+++ FG/src/components/Card.jsx
@@ -3,16 +3,22 @@ import { Link } from 'react-router-dom';
 
 const Card = ({data}) => {
 
-    const {companyName,companyLogo,minPrice,maxPrice,salaryType,jobLocation,employmentType,postingDate,description
+    const {companyName,jobTitle,companyLogo,minPrice,maxPrice,salaryType,jobLocation,employmentType,postingDate,description
     } = data;
   return (
     
         <section className='card'>
             <Link to ={"/"} className ="flex gap-4 flex-col sm:flex-row items-start">
                <img src= {companyLogo} alt=""/>
-                <div>
-                    
-                </div>
+              <div>
+                <h4 className = "text-black md-1 ">{companyName}</h4>
+                <h3 className="text-lg font-semibold mb-2">{jobTitle}</h3>
+              </div> 
+                 <div>
+                      <span>
+                        
+                      </span>
+                 </div>
 
               
             </Link>
