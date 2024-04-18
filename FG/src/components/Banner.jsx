8675a6b96f095986c1d79005fc1520ab10696da3
Bongani-4 <njathubongani@gmail.com>
2024-04-18 12:20:55 +0200

    animal cartaker set up input

--- FG/src/components/Banner.jsx
+++ FG/src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {FiSearch} from "react-icons/fi"
 
 const Banner = () => {
   return (
@@ -8,9 +9,13 @@ const Banner = () => {
 
      <form>
              <div>
-              <div className='flex md:rounded-s-md rounded shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-insert focus-within:ring-indigo-600 md:w-1/2 w-full'>
-                 <input type="text" name="title" id="title" placeholder='What role are you looking for ' className='block flex-1 border-0 bg-transparent py-1.5 pl-8 text=gray-900  placeholder:text=gray-400 
+              <div className='flex md:rounded-s-md rounded shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-insert md:w-1/2 w-full'>
+                 <input type="text" name="title" id="title" placeholder='What role are you looking for ' 
+                    className='block flex-1 border-0 bg-transparent py-1.5 pl-8 text-gray-900  placeholder:text-gray-400 
                   focus-right-0 sm:text-sm sm:leading-6' />
+                    
+                    <FiSearch  className='absolute mt-2.5 ml-2 text-gray-400 '/>
+
               </div>
              </div>
      </form>
