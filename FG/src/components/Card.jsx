c59d459039cb29aac658a7c0062c092b990a4fb7
Bongani-4 <njathubongani@gmail.com>
2024-04-18 16:58:57 +0200

    solve issues

--- FG/src/components/Card.jsx
+++ FG/src/components/Card.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const Card = ({data}) => {
+
+    const {companyName,companyLogo,minPrice,maxPrice,salaryType,jobLocation,employmentType,postingDate,description
+    } = data;
   return (
-    <div>{data.jobTitle}</div>
+    
+        <section className='card'>
+            <Link to ={"/"} className ="flex gap-4 flex-col sm:flex-row items-start">
+                
+
+            </Link>
+             
+        </section>
+     
   )
 }
 
