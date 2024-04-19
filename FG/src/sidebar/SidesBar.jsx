02ceab5af7c53ff06842c7d401734e2a8d14d140
Bongani-4 <njathubongani@gmail.com>
2024-04-19 18:19:27 +0200

    animal cartaker,salary filter

--- FG/src/sidebar/SidesBar.jsx
+++ FG/src/sidebar/SidesBar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Location from './Location';
+import Salary from './Salary';
 
 const SidesBar = ({ handleChange, handleClick }) => {
   return (
     <div className='space-y-5'>
       <h3 className='text-lg font-bold mb-2'>Filter</h3>
       <Location handleChange={handleChange} />
+      <Salary handleChange={handleChange} handleClick={handleClick} />
+
+
 
     </div>
   );
