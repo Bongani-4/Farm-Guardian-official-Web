2ebf8e7d5df387263ba21da4e8c8aa997dee8bed
Bongani-4 <njathubongani@gmail.com>
2024-04-20 11:44:38 +0200

    animal cartaker,emplyement filter

--- FG/src/sidebar/WorkExperience.jsx
+++ FG/src/sidebar/WorkExperience.jsx
@@ -4,19 +4,16 @@ import InputField from '../components/InputField'
 const WorkExperience = ({ handleChange }) => {
     return (
         <div>
-            <h4 className='text-lg font-medium mb-2'>Work experience</h4>
+            <h4 className='text-lg font-medium mb-2'>Employement Type</h4>
             <div>
                 <label className='sidebar-label-container'>
                     <input type='radio' name='test' id='test' value="" onChange={handleChange} />
-                    <span className='checkmark'> </span>Select All
+                    <span className='checkmark'> </span>Any
 
                 </label>
 
-                <InputField handleChange={handleChange} value="london" title="London" name="test" />
-                <InputField handleChange={handleChange} value="seattle" title="Seattle" name="test" />
-                <InputField handleChange={handleChange} value="madrid" title="Madrid" name="test" />
-                <InputField handleChange={handleChange} value="boston" title="Boston" name="test" />
-                <InputField handleChange={handleChange} value="Cape Town" title="Cape Town" name="test" />
+                <InputField handleChange={handleChange} value="Internship" title="Internship" name="test" />
+                <InputField handleChange={handleChange} value="seattle" title="Remote" name="test" />
 
             </div>
         </div>
