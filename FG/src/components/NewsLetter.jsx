ad8fe78bcf637207536e7e309adc8bba93ac5a03
Bongani-4 <njathubongani@gmail.com>
2024-04-20 13:01:17 +0200

    animal cartaker,newwsLetter

--- FG/src/components/NewsLetter.jsx
+++ FG/src/components/NewsLetter.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaEnvelopeOpenText } from 'react-icons/fa6'
+import { FaEnvelopeOpenText, FaRocket } from 'react-icons/fa6'
 
 const NewsLetter = () => {
     return (
@@ -13,9 +13,26 @@ const NewsLetter = () => {
                     <input type="email" name='email' id='email' placeholder='name@mail.com' className='w-full  block
                      py-2 pl-3 border focus:outline-none'/>
                     <input type='submit' value={"Subscribe"} className='w-full  block
-                     py-2 pl-3 border focus:outline-none bg-green rounded-sm text-white cursor-pointer'/>
+                     py-2 pl-3 border focus:outline-none bg-green rounded-sm text-white cursor-pointer font-semibold'/>
                 </div>
+
+
             </div>
+
+            <div>
+                <h3 className='text-lg font-bold mt-12 mb-2 flex items-center gap-2'>
+                    <FaRocket />
+                    Get noticed faster</h3>
+                <p className='text-black/75 text-base mb-4 '> Receive notifications and never miss out on new openings </p>
+                <div className='w-full space-y-4'>
+
+                    <input type='submit' value={"Upload your Resume "} className='w-full  block
+                     py-2 pl-3 border focus:outline-none bg-green rounded-sm text-white cursor-pointer font-semibold'/>
+                </div>
+
+
+            </div>
+
 
         </div>
     )
