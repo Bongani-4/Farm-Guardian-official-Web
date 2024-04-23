a79edfc88b1df4be4d39a8b23f8d5d57d47cc299
Bongani-4 <njathubongani@gmail.com>
2024-04-23 15:03:05 +0200

    postJobs

---
+++ FG/src/Pages/CreateJob.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateJob = () => {
+    return (
+        <div>post</div>
+    )
+}
+
+export default CreateJob
--- FG/src/Router/Router.jsx
+++ FG/src/Router/Router.jsx
@@ -1,25 +1,26 @@
 
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Home from "../Pages/Home";
 import App from "../App";
 import About from "../Pages/About";
 import AnimalCaretaker from "../Pages/AnimalCaretaker";
+import CreateJob from "../Pages/CreateJob";
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App/>,
-      children: [
-        {path: "/AnimalCaretaker", element: <AnimalCaretaker/> },
-        {path: "/", element: <Home/>},
-       
-       
-        
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/AnimalCaretaker", element: <AnimalCaretaker /> },
+      { path: "/", element: <Home /> },
+      { path: "/post-job", elemennt: <CreateJob /> }
 
-      
-      ]
-    },
-  ]);
 
-  export  default router;
 
-  
+
+
+    ]
+  },
+]);
+
+export default router;
+
