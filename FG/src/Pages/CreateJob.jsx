52909a9b7b45d12501e333ed4704e4111f1fd437
Bongani-4 <njathubongani@gmail.com>
2024-04-23 15:20:29 +0200

    initialize

--- FG/src/Pages/CreateJob.jsx
+++ FG/src/Pages/CreateJob.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CreateJob = () => {
     return (
-        <div>post</div>
+        <div className=' max-w-screen-2x1 container mx-auto xl:px-24 px-24'>screen</div>
     )
 }
 
--- FG/src/Router/Router.jsx
+++ FG/src/Router/Router.jsx
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/AnimalCaretaker", element: <AnimalCaretaker /> },
       { path: "/", element: <Home /> },
-      { path: "/post-job", elemennt: <CreateJob /> }
+      { path: "/post-job", element: <CreateJob /> }
 
 
 
