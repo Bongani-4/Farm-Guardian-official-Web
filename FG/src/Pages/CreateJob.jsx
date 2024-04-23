7e8d9915efd3a1b7eca53b76e48865209426d493
Bongani-4 <njathubongani@gmail.com>
2024-04-23 15:40:03 +0200

    react hook form

--- FG/src/Pages/CreateJob.jsx
+++ FG/src/Pages/CreateJob.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useForm } from "react-hook-form"
 
 const CreateJob = () => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => console.log(data)
+
+    console.log(watch("example")) // watch input value by passing the name of it
+
     return (
-        <div className=' max-w-screen-2x1 container mx-auto xl:px-24 px-24'>screen</div>
+        <div className=' max-w-screen-2x1 container mx-auto xl:px-24 px-24'>
+
+            {/**forms */}
+            <div className='bg-[#b3d39a] py-10px-4 lg:px-16'>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input defaultValue="test" {...register("example")} />
+
+                    <input {...register("exampleRequired", { required: true })} />
+                    {errors.exampleRequired && <span>This field is required</span>}
+
+                    <input type="submit" />
+                </form>F
+
+            </div>
+        </div>
     )
 }
 
