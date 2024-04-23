1eb073ce8f8bb6a7568e2876b7ff0fbed4f131d1
Bongani-4 <njathubongani@gmail.com>
2024-04-23 16:04:34 +0200

    postJobs forms

--- FG/src/Pages/CreateJob.jsx
+++ FG/src/Pages/CreateJob.jsx
@@ -16,17 +16,22 @@ const CreateJob = () => {
 
 
     return (
-        <div className=' max-w-screen-2x1 container mx-auto xl:px-24 px-24'>
+        <div className=' max-w-screen-2x1 mt-5 container mx-auto xl:px-24 px-24'>
 
             {/**forms */}
             <div className='bg-[#b3d39a] py-10px-4 lg:px-16'>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className='flex flex-col lg:flex-row items-center justify-between gap-8'>
-                        <div className='lg:w-1/2 w-full'>  </div>
+                        <div className='lg:w-1/2 w-full'>
+                            <label className='block mb-2 text-lg'>
+                                Job Title
+                            </label>
+                            <input type='text' placeholder='First name'{...register("First name"), { required: true, maxlength: 80 }}> </input>
+                        </div>
 
                     </div>
 
-                    <input type="submit" />
+                    <input type="submit" className='my-5' />
                 </form>
 
             </div>
