1779ec6b92184233be748c9fb0e727d39fe86a03
Bongani-4 <njathubongani@gmail.com>
2024-04-24 09:37:38 +0200

    postJobs forms

--- FG/src/Pages/CreateJob.jsx
+++ FG/src/Pages/CreateJob.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useForm } from "react-hook-form"
+import Navbar from '../components/Navbar'
 
 const CreateJob = () => {
     const {
@@ -16,25 +17,35 @@ const CreateJob = () => {
 
 
     return (
-        <div className=' max-w-screen-2x1 mt-5 container mx-auto xl:px-24 px-24'>
+        <div>
+            <Navbar />
+            <div className=' max-w-screen-2x1 mt-5 container mx-auto xl:px-24 px-24'>
 
-            {/**forms */}
-            <div className='bg-[#b3d39a] py-10 px-4 lg:px-16'>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className='flex flex-col lg:flex-row items-center justify-between gap-8'>
-                        <div className='lg:w-1/2 w-full'>
-                            <label className='block mb-2 text-lg'>
-                                Job Title
-                            </label>
-                            <input type='text' defaultValue={"Animal caretaker"} {...register("jobTitle")} className='block w-full flex-1 border-1 bg-white py-1.5 pl-3 text-gray-900
-                             placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6' />
+                {/**forms */}
+                <div className='bg-[#b3d39a] py-10 px-4 lg:px-16'>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className='flex flex-col lg:flex-row items-center justify-between gap-8'>
+                            <div className='lg:w-1/2 w-full'>
+                                <label className='block mb-2 text-lg'>
+                                    Job Title
+                                </label>
+                                <input type='text' defaultValue={"Animal caretaker"}
+                                    {...register("jobTitle")} className='create-job-input' />
+                            </div>
+                            <div className='lg:w-1/2 w-full'>
+                                <label className='block mb-2 text-lg'>
+                                    Company Name
+                                </label>
+                                <input type='text' placeholder='Ex: Farm Guardian'
+                                    {...register("companyName")} className='create-job-input' />
+                            </div>
+
                         </div>
 
-                    </div>
+                        <input type="submit" className='my-5' />
+                    </form>
 
-                    <input type="submit" className='my-5' />
-                </form>
-
+                </div>
             </div>
         </div>
     )
