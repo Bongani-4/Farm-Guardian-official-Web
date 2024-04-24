eeb8be7cc71912d1ae953bdf15aea1b634739fa7
Bongani-4 <njathubongani@gmail.com>
2024-04-24 17:42:48 +0200

    connect mongodb

--- FG-server/index.js
+++ FG-server/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000;
+ const cors = require('cors')
+const port = process.env.PORT || 3000;
+
+//middleware
+
+app.use(express.json)
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('ahh ta Bongs')
