51002d81a219cab80ab5299da5f3d4957c5a3912
Bongani-4 <njathubongani@gmail.com>
2024-04-25 17:40:45 +0200

    my job animal caretaker

---
+++ FG/src/Pages/MyJobs.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const MyJobs = () => {
+    return (
+        <div>
+
+            <Navbar />
+        </div>
+    )
+}
+
+export default MyJobs
--- FG/src/Router/Router.jsx
+++ FG/src/Router/Router.jsx
@@ -5,14 +5,16 @@ import App from "../App";
 import About from "../Pages/About";
 import AnimalCaretaker from "../Pages/AnimalCaretaker";
 import CreateJob from "../Pages/CreateJob";
+import MyJobs from "../Pages/MyJobs";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
       { path: "/AnimalCaretaker", element: <AnimalCaretaker /> },
-      { path: "/", element: <Home /> },
-      { path: "/post-job", element: <CreateJob /> }
+      { path: "/", element: <AnimalCaretaker /> },
+      { path: "/post-job", element: <CreateJob /> },
+      { path: "/my-job", element: <MyJobs /> }
 
 
 
