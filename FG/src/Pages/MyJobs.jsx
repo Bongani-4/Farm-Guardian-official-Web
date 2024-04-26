38e144fc21c0124e231b560fc6948d95b5510092
Bongani-4 <njathubongani@gmail.com>
2024-04-26 16:41:03 +0200

    my job portal, animal caretaker,table

--- FG/src/Pages/MyJobs.jsx
+++ FG/src/Pages/MyJobs.jsx
@@ -49,7 +49,7 @@ const MyJobs = () => {
                                         <h3 className="font-semibold text-base text-blueGray-700">All Jobs</h3>
                                     </div>
                                     <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
-                                        <button className="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">See all</button>
+                                        <button className="bg-[#06be06] text-white active:bg-green text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Post a New Job</button>
                                     </div>
                                 </div>
                             </div>
