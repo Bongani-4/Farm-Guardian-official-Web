5aa662a829e03700138e99816ada7d2800a31aa1
Bongani-4 <njathubongani@gmail.com>
2024-05-01 15:09:18 +0200

    LOGIGING SETUP

--- FG/src/Pages/LoginForm.jsx
+++ FG/src/Pages/LoginForm.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
 import { GoogleAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
-const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+const Googleprovider = new GoogleAuthProvider();
 const LoginForm = () => {
     return (
         <div>LoginForm</div>
