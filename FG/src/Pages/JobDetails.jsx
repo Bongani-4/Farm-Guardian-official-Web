b8ecedfdc64369fbf8e042bf36b2ab0a55478f52
Bongani-4 <njathubongani@gmail.com>
2024-05-01 15:47:18 +0200

    Job details

--- FG/src/Pages/JobDetails.jsx
+++ FG/src/Pages/JobDetails.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 const JobDetails = () => {
+    const { id } = useParams();
+    const [job, setJob] = useEffect([])
+    useEffect(() => {
+        fetch(`http://localhost:3000/all-jobs/${id}`).then(res => res.json).then(data => setJob(data))
+
+    }, [])
+
     return (
-        <div>JobDetails</div>
+        <div>JobDetails :{id}
+            <h1>
+
+            </h1>
+        </div>
     )
 }
 
