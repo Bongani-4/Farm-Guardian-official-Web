0fb8d7d9323b7168773c21ca76682a6cfe4586bf
Bongani-4 <njathubongani@gmail.com>
2024-05-06 16:53:59 +0200

    registration

--- FG/src/contexts/authContext/index.jsx
+++ FG/src/contexts/authContext/index.jsx
@@ -15,5 +15,32 @@ export function AuthProvider({ children }) {
         const unsubscribe = onAuthStateChanged(auth, initializeUser);
         return unsubscribe;
     }, [])
+
+    async function initializeUser(user) {
+        if (user) {
+
+            setCurrentUser({ ...user });
+            setUserLoggedIn(true);
+        }
+        else {
+            setCurrentUser(null);
+            setUserLoggedIn(false);
+        }
+
+        setLoading(false);
+
+    }
+
+    const value = {
+        currentUser,
+        userLoggedIn,
+        loading
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
 }
 
