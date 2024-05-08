7260f9b815114cdcb280c8d872b739f1ca00b2af
Bongani-4 <njathubongani@gmail.com>
2024-05-08 15:08:14 +0200

    landing page

--- FG/src/Pages/Home.jsx
+++ FG/src/Pages/Home.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import './LogIn.css';
-import farmGuardianLogo from '../assets/images/FarmGuardian.png';
-import featureImage from '../assets/images/featureFG-removebg.png';
+import React, { useState, useEffect } from "react";
+import { Navigation } from "../components/navigation";
+import { Header } from "../components/header";
+import { Features } from "../components/features";
+import { About } from "../components/about";
+import { Services } from "../components/services";
+import { Gallery } from "../components/gallery";
+import { Testimonials } from "../components/testimonials";
+import { Team } from "../components/Team";
+import { Contact } from "../components/contact";
+import JsonData from "../data/data.json";
+import SmoothScroll from "smooth-scroll";
+import "../App.css";
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
 const Home = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
   return (
-    <div className="hero">
-
-      <img src={farmGuardianLogo} className="logo" alt="Farm Guardian Logo" />
-
+    <div>
+      <Navigation />
+      <Header data={landingPageData.Header} />
+      <Features data={landingPageData.Features} />
+      <About data={landingPageData.About} />
+      <Services data={landingPageData.Services} />
+      <Gallery data={landingPageData.Gallery} />
+      <Testimonials data={landingPageData.Testimonials} />
+      <Team data={landingPageData.Team} />
+      <Contact data={landingPageData.Contact} />
     </div>
   );
 };
